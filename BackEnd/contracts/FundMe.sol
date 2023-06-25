173007591c0355ed2aba4d4c11b82c52dbcbc2f1
MohammadRokib <rokibk548@gmail.com>
2023-06-25 15:30:19 +0600

    added FundMe.sol

--- BackEnd/contracts/FundMe.sol
+++ BackEnd/contracts/FundMe.sol
@@ -3,10 +3,13 @@
 pragma solidity ^0.8.7;
 import "./PriceCoverter.sol";
 
+error FundMe__NotOwner();
+
 contract FundMe {
 
     using PriceConverter for uint256;
 
+    address private i_owner;
     address[] private s_funders;
     uint256 public MIN_USD = 50 * 1e18;
     AggregatorV3Interface private s_priceFeed;
@@ -14,6 +17,12 @@ contract FundMe {
 
     constructor(address s_priceFeedAddress) {
         s_priceFeed = AggregatorV3Interface(s_priceFeedAddress);
+        i_owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
     }
     
     function fund() public payable {
@@ -22,7 +31,29 @@ contract FundMe {
         s_addToAmount[msg.sender] += msg.value;
     }
     
-    function withdraw() public {
-
+    function withdraw() public onlyOwner{
+        uint256 private s = s_funders.length;
+        for (uint256 i = 0; i < s; i++) {
+            s_addToAmount[funders[i]] = 0;
+        }
+        s_funders = new address[](0);
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Transaction Failed");
     }
+
+    function getOwner() public view returns (address) {
+        return i_owner;
+    }
+
+    function getFunder(uint256 i) public view returns (address) {
+        return s_funders[i];
+    }
+
+    function getAddToAmount(address adr) public view returns (uint256) {
+        return s_addToAmount[adr];
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) [
+        return s_priceFeed;
+    ]
 }
